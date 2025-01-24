e730b9f322dd2b1c49049f67ce681fa9b4baebc9
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2025-01-24 14:39:16 -0300

    refactor: rename function and improve error handling for CEP fetching

--- script.ts
+++ script.ts
@@ -1,26 +1,27 @@
  
-const buscaroCep = async (): Promise<void> => {
+const buscarCep = async (): Promise<void> => {
     const cep = (document.getElementById('cep') as HTMLInputElement).value;
 
         if(cep?.length === 8 && !isNaN(Number(cep))){
             try{
             const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
             const data = await response.json();
-    
-                (document.getElementById('endereco') as HTMLInputElement).value = data.logradouro;
-                (document.getElementById('bairro') as HTMLInputElement).value = data.bairro;
-                (document.getElementById('cidade')as HTMLInputElement).value = data.localidade;
-                (document.getElementById('estado') as HTMLInputElement).value = data.uf;
-            }catch (error) {
-                alert('CEP NÃO ENCONTRADO!');
+                    const pegarValor = document.getElementById('id') as HTMLInputElement;
+                    
+                    pegarValor.value = data.logradouro;
+                    pegarValor.value = data.bairro;
+                    pegarValor.value = data.localidade;
+                    pegarValor.value = data.uf;
+
+            } catch (error) {
+                throw new Error('Erro na API');
              }
 
-        } else {
+        } else if (cep?.length !==8 && cep?.length === null) {
             alert('Digite um cep válido!');
         }
         }
         
 
-
-document.getElementById('cep')?.addEventListener('focusout', buscaroCep);   
+document.getElementById('cep')?.addEventListener('focusout', buscarCep);   
 
