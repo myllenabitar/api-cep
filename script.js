e5c74bf38e3c61879ed8bdd6047cc369bc366fb4
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2025-01-23 22:24:23 -0300

    validate CEP input and handle errors in fetching address data

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 "use strict";
 const buscaroCep = async () => {
     const cep = document.getElementById('cep').value;
-    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const data = await response.json();
-    document.getElementById('endereco').value = data.logradouro;
-    document.getElementById('bairro').value = data.bairro;
-    document.getElementById('cidade').value = data.localidade;
-    document.getElementById('estado').value = data.uf;
+    if (cep?.length === 8 && !isNaN(Number(cep))) {
+        try {
+            const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+            const data = await response.json();
+            document.getElementById('endereco').value = data.logradouro;
+            document.getElementById('bairro').value = data.bairro;
+            document.getElementById('cidade').value = data.localidade;
+            document.getElementById('estado').value = data.uf;
+        }
+        catch (error) {
+            alert('CEP NÃO ENCONTRADO!');
+        }
+    }
+    else {
+        alert('Digite um cep válido!');
+    }
 };
 document.getElementById('cep')?.addEventListener('focusout', buscaroCep);
--- script.ts
+++ script.ts
@@ -1,13 +1,26 @@
  
 const buscaroCep = async (): Promise<void> => {
     const cep = (document.getElementById('cep') as HTMLInputElement).value;
-    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const data = await response.json();
+
+        if(cep?.length === 8 && !isNaN(Number(cep))){
+            try{
+            const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+            const data = await response.json();
     
-        (document.getElementById('endereco') as HTMLInputElement).value = data.logradouro;
-        (document.getElementById('bairro') as HTMLInputElement).value = data.bairro;
-        (document.getElementById('cidade')as HTMLInputElement).value = data.localidade;
-        (document.getElementById('estado') as HTMLInputElement).value = data.uf;
-    }
+                (document.getElementById('endereco') as HTMLInputElement).value = data.logradouro;
+                (document.getElementById('bairro') as HTMLInputElement).value = data.bairro;
+                (document.getElementById('cidade')as HTMLInputElement).value = data.localidade;
+                (document.getElementById('estado') as HTMLInputElement).value = data.uf;
+            }catch (error) {
+                alert('CEP NÃO ENCONTRADO!');
+             }
+
+        } else {
+            alert('Digite um cep válido!');
+        }
+        }
+        
+
+
 document.getElementById('cep')?.addEventListener('focusout', buscaroCep);   
 
