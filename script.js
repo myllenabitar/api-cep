62099dfe0f438a994525fafff52b6159019a37f1
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2025-01-23 21:49:02 -0300

    update form title, enhance CEP fetching functionality, and adjust CSS alignment

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "use strict";
-const buscaroCep = () => {
-    const cep = document.getElementById('cep');
-    console.log('cep');
+const buscaroCep = async () => {
+    const cep = document.getElementById('cep').value;
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    document.getElementById('endereco').value = data.logradouro;
+    document.getElementById('bairro').value = data.bairro;
+    document.getElementById('cidade').value = data.localidade;
+    document.getElementById('estado').value = data.uf;
 };
-document.getElementById('cep')?.addEventListener('focus', buscaroCep);
+document.getElementById('cep')?.addEventListener('focusout', buscaroCep);
--- script.ts
+++ script.ts
@@ -1,8 +1,13 @@
  
-const buscaroCep = (): void => {
-    const cep = document.getElementById('cep') as HTMLInputElement;
-    console.log('cep');
-}
-
-document.getElementById('cep')?.addEventListener('focus', buscaroCep);   
+const buscaroCep = async (): Promise<void> => {
+    const cep = (document.getElementById('cep') as HTMLInputElement).value;
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    
+        (document.getElementById('endereco') as HTMLInputElement).value = data.logradouro;
+        (document.getElementById('bairro') as HTMLInputElement).value = data.bairro;
+        (document.getElementById('cidade')as HTMLInputElement).value = data.localidade;
+        (document.getElementById('estado') as HTMLInputElement).value = data.uf;
+    }
+document.getElementById('cep')?.addEventListener('focusout', buscaroCep);   
 
